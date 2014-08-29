3ef6cb37a9c6603e1bfc591c1ab50f3539274e13
Scott Meade <scott@synapsoftware.com>
2014-08-29 14:53:59 -0600

    Add reviews resource.
    Non-ajax. Standard routes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829202212) do
+ActiveRecord::Schema.define(version: 20140829204319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,5 +26,15 @@ ActiveRecord::Schema.define(version: 20140829202212) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "product_id"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
 
 end
